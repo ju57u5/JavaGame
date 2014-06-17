b82bbe3dad731b7cee1e7200f4cb9e103e1cfca6
ju57u5 <geldtransporter@hotmail.de>
2014-06-17 21:28:56 +0200

    Klasse für die Spieler erstellt.

--- Applet/JavaGame.java
+++ Applet/JavaGame.java
@@ -1,7 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.applet.Applet;
-
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 /**
   *
   * Beschreibung
@@ -10,21 +13,54 @@ import java.applet.Applet;
   * @author 
   */
 
-public class JavaGame extends Applet {
-  // Anfang Attribute
-  // Ende Attribute
+public class JavaGame extends Applet implements KeyListener {
+  
+  String appletpfad = (System.getProperty("user.dir")+"\texture\test.png");
+  
+  //File texture = new File(appletpfad); 
+  File texture = new File("Z:\\test.png");
+  Player player1 = new Player(texture,texture);
   
   public void init() {
-    Panel cp = new Panel(null);
-    cp.setBounds(0, 0, 300, 300);
-    add(cp);
-    // Anfang Komponenten
     
-    // Ende Komponenten
+    
     
   } // end of init
-
-  // Anfang Methoden
-  // Ende Methoden
+  
+  public void paint (Graphics g) {
+    player1.laden(this,g);
+    
+  }  
   
 } // end of class JavaGame
+
+class Player extends Thread implements KeyListener{
+  
+  File playertexture,shottexture;
+  BufferedImage textureImage = new BufferedImage(1000,1000,1);
+  Graphics g;
+  
+  public Player(File playertexture, File shottexture) {
+    this.playertexture = playertexture;
+    this.shottexture = shottexture;
+    //this.g = g;
+    addkeyListener();
+    
+  }  
+  
+  public void laden(JavaGame Game,Graphics g) {
+    
+    try { 
+      textureImage = ImageIO.read(playertexture);
+    } catch(IOException exeption) {
+      
+    }
+    
+  }
+  
+  public boolean keyPressed(KeyEvent e) {
+    int key = e.getKeyChar();
+    
+  }  
+  
+}
