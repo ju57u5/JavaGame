c74340f7dbe5f29dc1031347816692a6f2658f37
ju57u5 <geldtransporter@hotmail.de>
2014-06-17 19:29:14 +0200

    Applet erstellt.

---
+++ Applet/JavaGame.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.Applet;
+
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 17.06.2014
+  * @author 
+  */
+
+public class JavaGame extends Applet {
+  // Anfang Attribute
+  // Ende Attribute
+  
+  public void init() {
+    Panel cp = new Panel(null);
+    cp.setBounds(0, 0, 300, 300);
+    add(cp);
+    // Anfang Komponenten
+    
+    // Ende Komponenten
+    
+  } // end of init
+
+  // Anfang Methoden
+  // Ende Methoden
+  
+} // end of class JavaGame
