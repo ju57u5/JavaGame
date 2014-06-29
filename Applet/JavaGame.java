36cd2d2b96aa2bd1f19bebdfdbe01bfde6bbe96c
ju57u5 <geldtransporter@hotmail.de>
2014-06-29 19:15:09 +0200

    Player inverted geadded

--- Applet/JavaGame.java
+++ Applet/JavaGame.java
@@ -19,10 +19,12 @@ public class JavaGame extends Applet implements KeyListener {
   boolean notrunning = true;
   //File texture = new File(appletpfad);
   URL  PlayerTextureUrl;
-  File texture = new File("J:\\test.png");
+  File texture = new File("K:\\test.png");
+  File shottexture = new File("K:\\shot.png");
   Player player[] = new Player[3];
   Image dbImage;
   Graphics dbGraphics;
+  damageLogig DamageLogig;
   //Erzaehler erz = new Erzaehler(player1, player2);
   
   public void init() {
@@ -30,8 +32,8 @@ public class JavaGame extends Applet implements KeyListener {
     dbGraphics = dbImage.getGraphics();
     
     
-    player[1] = new Player(texture,texture,dbImage,KeyEvent.VK_LEFT,KeyEvent.VK_RIGHT,KeyEvent.VK_UP,KeyEvent.VK_SHIFT);                // I'm in Space! SPACE!
-    player[2] = new Player(texture,texture,dbImage,KeyEvent.VK_A,KeyEvent.VK_D,KeyEvent.VK_W,KeyEvent.VK_Q);
+    player[1] = new Player(texture,shottexture,dbImage,KeyEvent.VK_LEFT,KeyEvent.VK_RIGHT,KeyEvent.VK_UP,KeyEvent.VK_SHIFT);                // I'm in Space! SPACE!
+    player[2] = new Player(texture,shottexture,dbImage,KeyEvent.VK_A,KeyEvent.VK_D,KeyEvent.VK_W,KeyEvent.VK_Q);
     
     try {
       PlayerTextureUrl = new URL("http://www.mariowiki.com/images/5/57/WaluigiMP8Official.png");
@@ -58,16 +60,16 @@ public class JavaGame extends Applet implements KeyListener {
   
   
   
-  
   public void paint (Graphics g) {
     
     if (notrunning) {
-      player[1].laden(this,dbGraphics,g,0,0);
-      player[2].laden(this,dbGraphics,g,100,100);    
+      player[1].laden(this,dbGraphics,g,0,100);
+      player[2].laden(this,dbGraphics,g,300,100);    
       this.addKeyListener(player[1]);
       this.addKeyListener(player[2]);
       
       GameRunner gamerunner = new GameRunner(player,this);
+      DamageLogig = new damageLogig (gamerunner);
       
       notrunning = false;
     } // end of if
@@ -80,16 +82,18 @@ public class JavaGame extends Applet implements KeyListener {
 class Player extends Thread implements KeyListener  {
   
   File playertexture,shottexture;
-  BufferedImage textureImage = new BufferedImage(1000,1000,1);
+  BufferedImage textureImage, textureImageb = new BufferedImage(1000,1000,1);
+  
   Graphics g,db;
   KeyEvent taste;
   JavaGame Game;
   Image dbImage;
-  boolean firsttimepressed=false;
+  boolean characterInverted,firsttimepressed,freezeControls=false;
   int x,y=0;                                                                                                                  //Positionen
   int left,right,jump,attack;                                                                                                 //Tasten
   int jumpup, jumpdown = 0;                                                                                                   //Jump Vars
   boolean[] keys = new boolean[1000];
+  int health = 100;
   
   public Player(File playertexture, File shottexture, Image dbImage, int left, int right , int jump, int attack) {
     this.playertexture = playertexture;
@@ -121,19 +125,47 @@ class Player extends Thread implements KeyListener  {
       
     }
     
-    
+    textureImageb = verticalflip(textureImage);
   }
+  
+  public BufferedImage verticalflip(BufferedImage img) {
+    int w = img.getWidth();
+    int h = img.getHeight();
+    BufferedImage dimg = new BufferedImage(w, h, img.getType());
+    Graphics2D g = dimg.createGraphics();
+    /**
+    * img - the specified image to be drawn. This method does nothing if
+    * img is null. dx1 - the x coordinate of the first corner of the
+    * destination rectangle. dy1 - the y coordinate of the first corner of
+    * the destination rectangle. dx2 - the x coordinate of the second
+    * corner of the destination rectangle. dy2 - the y coordinate of the
+    * second corner of the destination rectangle. sx1 - the x coordinate of
+    * the first corner of the source rectangle. sy1 - the y coordinate of
+    * the first corner of the source rectangle. sx2 - the x coordinate of
+    * the second corner of the source rectangle. sy2 - the y coordinate of
+    * the second corner of the source rectangle. observer - object to be
+    * notified as more of the image is scaled and converted.
+    *
+    */
+    g.drawImage(img, 0, 0, w, h, w, 0, 0, h, null);
+    g.dispose();
+    return dimg;
+  }
+  
+  
   
   public void updateKey()
   {
-    if (firsttimepressed) {
+    if (firsttimepressed && !freezeControls) {
       
       if (keys[right] && x<900) {                              
         x +=5;
+        characterInverted = false;
       } // end of if
       
       if (keys[left] && x>0) {                                 
-        x -=5; 
+        x -=5;
+        characterInverted = true; 
       } // end of if
       
       if (keys[jump] && y>0) {                                 
@@ -141,22 +173,30 @@ class Player extends Thread implements KeyListener  {
       } // end of if
       
       if (keys[attack] && y<900) {                             
-        y +=5; 
+        Shot bullet = new Shot(shottexture,!characterInverted, 5, Game, this);
+        bullet.laden(x,y);
       } // end of if
     } // end of if  
     
     updateJump(10);
-    Game.dbImage.getGraphics().drawImage(textureImage,x,y,Game);
+    if (!characterInverted) {
+      Game.dbImage.getGraphics().drawImage(textureImage,x,y,Game);
+    } // end of if
+    else {
+      Game.dbImage.getGraphics().drawImage(textureImageb,x,y,Game);
+    } // end of if-else
     
-    System.out.println(x+" "+y);
     
   }
   
   public void run() {
-    
-    
-    
-    
+  }
+  
+  public void setDamage(int damage) {
+    health -= damage;
+    if (health < 0) {
+      freezeControls = true;
+    } // end of if
   }  
   
   public void keyPressed(KeyEvent e) 
@@ -204,19 +244,25 @@ class Player extends Thread implements KeyListener  {
 
 class Shot {
   
-  int x,y;
+  int x,y,speed;
   JavaGame Game;
   File shottexture;
+  Player owner;
+  boolean rechts;
   BufferedImage textureImage = new BufferedImage(1000,1000,1);
   
-  public Shot (File shottexture) {
+  
+  public Shot (File shottexture, boolean rechts, int speed, JavaGame Game, Player owner) {
     this.shottexture = shottexture;
-    
+    this.Game = Game;
+    this.speed = speed;
+    this.rechts = rechts;
+    this.owner = owner;
   }  
   
-  public void laden(JavaGame Game, int x, int y) {
+  public void laden(int x, int y) {
     
-    this.Game = Game;
+    // this.Game = Game;
     this.x = x;
     this.y = y;
     
@@ -226,27 +272,59 @@ class Shot {
       
     }
     
-    
+    Game.DamageLogig.registerShot(this);            //Registrierung
   }
   
   public void updateShot() {
+    if (rechts) {
+      x += speed;
+      
+    } // end of if
+    else {
+      x -= speed;
+      
+    } // end of if-else
     
-    
-    
-    
+    Game.dbImage.getGraphics().drawImage(textureImage,x,y,Game);
     
   }  
 }
 
 class damageLogig {
+  Shot shot[] = new Shot[100];
+  GameRunner runner;
+  int counter = 0;
+  int xDistance,yDistance;
   
-  public void registerShot() {
+  public damageLogig(GameRunner runner) {
+    this.runner = runner;
     
+  }  
+  
+  public void registerShot(Shot bullet) {
+    shot[counter] = bullet;
+    runner.shot[counter] = bullet;
     
+    runner.isthereshot = true;
+    counter++;
   }
   
   public void updateDamage() {  
     
+    for (int counter=1;counter < runner.player.length;counter++ ) {
+      for (int counterb = 0;counterb < runner.shot.length;counterb++) {
+        if (runner.shot[counterb] != null && runner.shot[counterb].owner != runner.player[counter]) {
+          
+          xDistance = runner.shot[counterb].x - runner.player[counter].x;
+          yDistance = runner.shot[counterb].y - runner.player[counter].y ;
+          
+          if ( (xDistance > -50 && xDistance <67) && (yDistance > -50 && yDistance < 100) ) {
+            runner.player[counter].setDamage(10);
+          } // end of if
+          
+        } // end of if
+      } // end of for
+    } // end of for
     
   }  
 }  
@@ -278,7 +356,9 @@ class Erzaehler extends Observable {
 
 class GameRunner extends Thread {
   Player player[] = null;
+  Shot shot[] = new Shot[100];
   JavaGame Game ;
+  boolean isthereshot = false;
   
   public GameRunner (Player[] player, JavaGame Game) {
     this.player = player;
@@ -301,6 +381,16 @@ class GameRunner extends Thread {
         player[counter].updateKey(); 
         
       }
+      if (isthereshot) {
+        for (int counter=0;counter<shot.length;counter++)
+        { 
+          if (shot[counter] != null) {
+            shot[counter].updateShot();  
+          } // end of if
+        }
+        Game.DamageLogig.updateDamage();
+      } // end of if
+      
       Game.getGraphics().drawImage(Game.dbImage,0,0,Game);  
     } // end of while
     
