d64e599ee2b540973b5043845831e67c03ba7626
christianger <chr.koehlke@googlemail.com>
2014-07-20 12:42:42 +0200

    Bot können geFREEZEt werden

--- Applet/Bot.java
+++ Applet/Bot.java
@@ -104,6 +104,77 @@ class Bot extends Player {
         Game.dbImage.getGraphics().drawImage(boomImage,boomx,boomy,Game);
       } // end of if
       
+      if (gefroren<0) {
+        freezeControls=false;
+        if (x<100 || x>1000) {      //selbstschutz
+          if (x<100)
+          {x += speed;
+            characterInverted = false;
+          }
+          if (x>1000)
+          {x -= speed;
+            characterInverted = true;
+          }
+        } 
+        else {
+          
+          if (Game.player[angriffsziel].health>0 && Game.player[angriffsziel] != this) {
+            int botdif=y-Game.player[angriffsziel].y;      //Kampf gegen Spieler 2
+            
+            if (x<Game.player[angriffsziel].x && Game.player[angriffsziel].x<1000) {
+              x +=speed/2;
+              characterInverted = false;
+              
+            } // end of if
+            if (x>Game.player[angriffsziel].x && Game.player[angriffsziel].x>100) {
+              x -=speed/2;
+              characterInverted = true; 
+            } // end of if 
+            
+            if (x==Game.player[angriffsziel].x && y<Game.player[angriffsziel].y) {
+              y -=1;
+              justupdated = true;
+              aufeinerebeneüberjemandem=false;                              /////Runterfallen
+            } // end of if
+            
+            
+            if (x==Game.player[angriffsziel].x && botdif<80 && botdif>-80) {
+              if (schusssperre == 0) {
+                Shot bullet = new Shot(shottexture,!characterInverted, 10, Game,this);
+                bullet.laden(x,y+50);
+                schusssperre = sperrzeit;
+              } // end of if
+            }
+            if (botdif<80 && botdif>-80) {     //selbe Höhe
+              
+              if (schusssperre == 0) {
+                Shot bullet = new Shot(shottexture,!characterInverted, 10, Game,this);
+                bullet.laden(x,y+50);
+                schusssperre = sperrzeit;
+              } // end of if
+              
+            } // end of if
+            
+            if (y==Game.player[angriffsziel].y && x==Game.player[angriffsziel].x) {
+              aufeinerebeneüberjemandem=true;
+            } // end of if
+            
+            if (y>Game.player[angriffsziel].y) {
+              setJump(jumpheigth);
+            } // end of if
+            
+          } 
+          
+        }
+      } // end of if
+      if (gefroren>0) {
+        freezeControls=true;
+        Game.dbImage.getGraphics().drawImage(freezeImage,x-5,y,Game);
+        Game.dbImage.getGraphics().drawString("Gefroren   "+gefroren/10,xHealth,yHealth+115);
+        
+      } // end of if
+      gefroren-=1;
+      
       min=1000000;  
       for (int c=1;c<Game.player.length;c++) {
         if (Game.player[c] != null) {
@@ -119,66 +190,7 @@ class Bot extends Player {
       
       
       
-      if (x<100 || x>1000) {      //selbstschutz
-        if (x<100)
-        {x += speed;
-          characterInverted = false;
-        }
-        if (x>1000)
-        {x -= speed;
-          characterInverted = true;
-        }
-      } 
-      else {
-        
-        if (Game.player[angriffsziel].health>0 && Game.player[angriffsziel] != this) {
-          int botdif=y-Game.player[angriffsziel].y;      //Kampf gegen Spieler 2
-          
-          if (x<Game.player[angriffsziel].x && Game.player[angriffsziel].x<1000) {
-            x +=speed/2;
-            characterInverted = false;
-            
-          } // end of if
-          if (x>Game.player[angriffsziel].x && Game.player[angriffsziel].x>100) {
-            x -=speed/2;
-            characterInverted = true; 
-          } // end of if 
-          
-          if (x==Game.player[angriffsziel].x && y<Game.player[angriffsziel].y) {
-            y -=1;
-            justupdated = true;
-            aufeinerebeneüberjemandem=false;                              /////Runterfallen
-          } // end of if
-          
-          
-          if (x==Game.player[angriffsziel].x && botdif<80 && botdif>-80) {
-            if (schusssperre == 0) {
-              Shot bullet = new Shot(shottexture,!characterInverted, 10, Game,this);
-              bullet.laden(x,y+50);
-              schusssperre = sperrzeit;
-            } // end of if
-          }
-          if (botdif<80 && botdif>-80) {     //selbe Höhe
-            
-            if (schusssperre == 0) {
-              Shot bullet = new Shot(shottexture,!characterInverted, 10, Game,this);
-              bullet.laden(x,y+50);
-              schusssperre = sperrzeit;
-            } // end of if
-            
-          } // end of if
-          
-          if (y==Game.player[angriffsziel].y && x==Game.player[angriffsziel].x) {
-            aufeinerebeneüberjemandem=true;
-          } // end of if
-          
-          if (y>Game.player[angriffsziel].y) {
-            setJump(jumpheigth);
-          } // end of if
-          
-        } 
-        
-      }
+      
     }
   }
   
